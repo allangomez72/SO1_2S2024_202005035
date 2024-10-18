22bbfdc7619493cb0f3b3bb47e8ad6da2926d7d8
allan <allantime72@gmail.com>
2024-10-18 03:38:23 -0600

    inicio

---
+++ Proyecto2/gRPC/agronomia/client-whit-go/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	pb "client-whit-go/proto-go"
+	"context"
+	"github.com/gofiber/fiber/v2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"time"
+)
+
+// se hace el struct de como van a llegar las solicitudes HTTP y por eso se va usar fiber para mas simplicidad
+type Data struct {
+	Name       string `json:"name"`
+	Age        int32  `json:"age"`
+	Faculty    string `json:"faculty"`
+	Discipline int32  `json:"discipline"`
+}
+
+// metodo para enviar los datos al server
+func sendData_To_Server(fiberCtx *fiber.Ctx) error {
+	var body Data
+	if err := fiberCtx.BodyParser(&body); err != nil {
+		return fiberCtx.Status(400).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	//Establecer la conexion gRPC con el servidor
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials())) //para cuando ya tenga las rutas de los deploymets cambiar
+	if err != nil {
+		log.Fatalf("No se puede conectar: %v", err)
+	}
+	defer conn.Close()
+
+	//Crear un cliente gRPC
+	client := pb.NewFacultadServiceClient(conn)
+
+	// Crear un canal para recibir las respuestas y errores
+	responseChann := make(chan *pb.StudentResponse) // para las respuestas de servidor
+	errorChann := make(chan error)                  // para manejar cualquier error
+
+	//ejecutar gorutine para enviar la solicitud y recivir la respuesta
+	go func() {
+
+		//Enviar solicitud de Student al servidor gRPC y escribir la respuesta
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		ret, err := client.SendUserInfo(ctx, &pb.Student{
+			Name:       body.Name,
+			Age:        int32(body.Age),
+			Faculty:    body.Faculty,
+			Discipline: int32(body.Discipline),
+		})
+
+		if err != nil {
+			errorChann <- err
+			return
+		}
+
+		responseChann <- ret
+	}()
+
+	select {
+	case response := <-responseChann:
+		return fiberCtx.JSON(fiber.Map{
+			"message": response.GetMessage(),
+		})
+	case err := <-errorChann:
+		return fiberCtx.Status(500).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	case <-time.After(5 * time.Second):
+		return fiberCtx.Status(500).JSON(fiber.Map{
+			"errror": "timeout",
+		})
+	}
+
+}
+
+func main() {
+	//Crear la app de FIber
+	app := fiber.New()
+
+	//definir cual es el endpoint que va a recibir los datos del estudiante
+	app.Post("/sendstudent", sendData_To_Server)
+
+	//Iniciar el servidor en el puerto 3000
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatalf("Error al iniciar el servidor  %v", err)
+	}
+}
